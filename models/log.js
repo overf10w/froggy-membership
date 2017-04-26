0883fb180cf213c23783a946c44c617baa588236
pwnzr <pvvner@yandex.ru>
2017-04-26 12:31:48 +0300

    Add logging object

---
+++ models/log.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+var Log = function (args) {
+  assert.ok(args.subject && args.entry && args.userId, "Need subj, entry and userId");
+  var log = {};
+  log.subject = args.subject;
+  log.entry = args.entry;
+  log.createdAt = new Date();
+  log.userId = args.userId;
+
+  return log;
+
+};
